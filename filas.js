90620eedaa1e67b1dc21eb03a8a31311171124fa
marcelldac <dacmarcell@gmail.com>
2023-12-01 18:04:23 -0300

    jogo batata quente

--- filas.js
+++ filas.js
@@ -36,7 +36,10 @@ queue.enqueue("Natália");
 queue.enqueue("Django");
 queue.dequeue();
 console.log(queue.print());
- */
+*/
+
+/* 
+//#region Priority Queue
 
 class PriorityQueue {
   constructor() {
@@ -83,3 +86,30 @@ pqueue.enqueue("Django", 1);
 pqueue.enqueue("João", 0);
 pqueue.enqueue("Maria", 3);
 pqueue.print();
+
+//#endregion
+ */
+
+function hotPotato(namelist, num) {
+  var queue = new Queue();
+
+  for (let i = 0; i < namelist.length; i++) {
+    queue.enqueue(namelist[i]);
+  }
+
+  var eliminated = "";
+
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+    eliminated = queue.dequeue();
+    console.log(eliminated, "was eliminated from hot potato game");
+  }
+
+  return queue.dequeue();
+}
+
+var names = ["Marcell", "Natália", "Django"];
+var winner = hotPotato(names, 7);
+console.log("The winner is: " + winner);
