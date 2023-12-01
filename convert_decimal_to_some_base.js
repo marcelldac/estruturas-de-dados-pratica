75f20e24ad24a606661d42d8e97ab533f44e29c9
marcelldac <dacmarcell@gmail.com>
2023-12-01 17:08:48 -0300

    conversor de de decimal pra qualquer base

---
+++ convert_decimal_to_some_base.js
@@ -0,0 +1,20 @@
+function baseConverter(decimalNumber, base) {
+  var restStack = [],
+    rest,
+    baseString = "",
+    digits = "0123456789ABCDEF";
+
+  while (decimalNumber > 0) {
+    rest = Math.floor(decimalNumber % base);
+    restStack.push(rest);
+    decimalNumber = Math.floor(decimalNumber / base);
+  }
+
+  while (restStack.length > 0) {
+    baseString += digits[restStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter(123, 2)); //base hexadecimal
