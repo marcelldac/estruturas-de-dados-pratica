9a1e17aad75db806cfcfb5fa7499848209624052
marcelldac <dacmarcell@gmail.com>
2023-12-01 18:09:26 -0300

    adiciona linked list - lista ligada

---
+++ linked_list.js
@@ -0,0 +1,39 @@
+//Listas ligadas | Ponteiros | Ex.: Trem
+
+function linkedList() {
+  var Node = function (element) {
+    this.element = element;
+    this.next = null;
+  };
+
+  var length = 0;
+  var head = null;
+
+  this.append = function (element) {
+    //Adiciona um elemento ao fim da lista.
+  };
+
+  this.insert = function (position, element) {
+    //Adiciona um elemento em uma posição específica.
+  };
+
+  this.removeAt = function (position) {
+    //Remove o elemento de uma posição específica.
+  };
+
+  this.remove = function (element) {
+    //Remove o elemento element.
+  };
+
+  this.indexOf = function (element) {
+    //Retorna a posição do elemento.
+  };
+
+  this.isEmpty = function () {
+    //Retorna se a lista está vazia ou não.
+  };
+
+  this.print = function () {
+    //Imprime no console
+  };
+}
