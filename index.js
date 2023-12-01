39a5681fe6ed00d312733f76c3aa9acf4ff82c3c
marcelldac <dacmarcell@gmail.com>
2023-12-01 15:34:55 -0300

    top secret repo initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const arr1 = new Array(1, 2, 3, 4, 5);
+const arr2 = new Array(5, 4, 3, 2, 1);
+const arr3 = new Array();
+
+arr3[0] = arr1;
+arr3[1] = arr2;
+//Iterando um array
+for (let i = 0; i < arr3.length; i++) {
+  for (let j = 0; j < arr3[i].length; j++) {
+    console.log(arr3[i][j]);
+  }
+}
