48d041cccdf17326a51c0d9d3c5c7d4dddda8671
Developer Marcell <dacmarcell@gmail.com>
2024-01-13 13:45:12 -0300

    Update and rename convert_decimal_to_binary.js to decimal2binary.js

--- decimal2binary.js
+++ decimal2binary.js
@@ -2,18 +2,13 @@ function decimalPBinario(numeroDecimal) {
   var pilhaDeRestos = [],
     restoDivisao,
     stringBinaria = "";
-
   while (numeroDecimal > 0) {
     restoDivisao = Math.floor(numeroDecimal % 2);
     pilhaDeRestos.push(restoDivisao);
     numeroDecimal = Math.floor(numeroDecimal / 2);
   }
-
   while (pilhaDeRestos.length > 0) {
     stringBinaria += pilhaDeRestos.pop().toString();
   }
-
   return stringBinaria;
 }
-
-console.log(decimalPBinario(10));
