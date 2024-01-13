8869825b612f376c66dcd6145ff2ed997af634ee
Developer Marcell <dacmarcell@gmail.com>
2024-01-13 13:44:48 -0300

    Update array_tridiciomensional.js

--- array_tridiciomensional.js
+++ array_tridiciomensional.js
@@ -1,20 +1,13 @@
-/* Array tridimensional */
-
 var month = [];
-
 var firstWeeks = [];
 var lastWeeks = [];
-
 var avgTempWeek1 = [1, 2, 3, 4, 5];
 var avgTempWeek2 = [5, 4, 3, 2, 1];
 var avgTempWeek3 = [6, 7, 8, 9, 0];
 var avgTempWeek4 = [10, 11, 12, 13, 14];
-
 firstWeeks = [avgTempWeek1, avgTempWeek2];
 lastWeeks = [avgTempWeek3, avgTempWeek4];
-
 month = [firstWeeks, lastWeeks];
-
 for (let i = 0; i < month.length; i++) {
   for (let j = 0; j < month[i].length; j++) {
     for (let k = 0; k < month[i][j].length; k++) {
